abb2b69869090b6e8c1784f204b4e0daf0c48657
dmitry <dmitrychurkin1988@gmail.com>
2020-03-30 23:49:11 +0300

    unfreezed preloader for demonstration

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,9 @@ import React, { memo, FC } from "react";
 import Preloader from "./Preloader";
 import Landing from "./Landing";
 
-// TODO: remove it later!!!!!!
-const SHOW = false;
 const App: FC = () => (
   <>
-    {SHOW && <Preloader />}
+    <Preloader />
     <Landing />
   </>
 );
