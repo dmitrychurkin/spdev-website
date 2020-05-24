380eb9bc43c4b94950b51b4151a9a669b8e9a6ef
dmitry <dmitrychurkin1988@gmail.com>
2020-05-24 16:13:51 +0300

    #12 - investigate x-forwarded-for header

--- utils/remoteIp.ts
+++ utils/remoteIp.ts
@@ -2,6 +2,7 @@ import { NextApiRequest } from "next";
 
 export default (req: NextApiRequest) => {
   const ipAddr = req.headers["x-forwarded-for"];
+  console.log(`req.headers["x-forwarded-for"] ipAddr => `, ipAddr);
   if (typeof ipAddr === "string") {
     return ipAddr.split(",").slice(-1)[0];
   }
